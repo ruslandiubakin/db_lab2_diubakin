b55a4db5ff3ef8d9a8eab789601a419f254a9a3e
Ruslan Diubakin <72011064+ruslandiubakin@users.noreply.github.com>
2022-12-13 17:17:57 +0200

    lab2

---
+++ create.sql
@@ -0,0 +1,26 @@
+CREATE TABLE
+    countries(
+        country_iso2 char(10) NOT NULL,
+        country_name char(50) NOT NULL,
+        PRIMARY KEY(country_iso2)
+    );
+
+CREATE TABLE
+    shows(
+        show_id int NOT NULL,
+        show_title char(100) NOT NULL,
+        season_title char(100),
+        category char(50),
+        PRIMARY KEY(show_id)
+    );
+
+CREATE TABLE
+    top10_by_contries(
+        country_iso2 char(10) NOT NULL,
+        show_id int NOT NULL,
+        week DATE,
+        weekly_rank int,
+        cumulative_weeks_in_top_10 int,
+        Foreign Key (country_iso2) REFERENCES countries(country_iso2),
+        Foreign Key (show_id) REFERENCES shows(show_id)
+    );
